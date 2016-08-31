d83bf5e6effec83519ce96c98de40f8415abc04a
Mark Soper <msoper@vmware.com>
2016-08-31 16:53:01 -0400

    rename proj

--- plugin.js
+++ plugin.js
@@ -1,14 +1,14 @@
 define([
    'angular',
-   'plugins/esxui-example-plugin/scripts/controllers/example-main.js',
-   'plugins/esxui-example-plugin/scripts/controllers/example-tab-one.js',
-   'plugins/esxui-example-plugin/scripts/controllers/example-tab-two.js',
-   'plugins/esxui-example-plugin/scripts/controllers/wizard/test-page-one.js',
-   'plugins/esxui-example-plugin/scripts/controllers/wizard/test-page-two.js',
-   'plugins/esxui-example-plugin/scripts/services/example.js',
-   'plugins/esxui-example-plugin/scripts/services/example-dialog.js',
-   'plugins/esxui-example-plugin/scripts/services/example-wizard.js',
-   'plugins/esxui-example-plugin/scripts/services/example-context-menu.js'
+   'plugins/docker-volume-plugin/scripts/controllers/example-main.js',
+   'plugins/docker-volume-plugin/scripts/controllers/example-tab-one.js',
+   'plugins/docker-volume-plugin/scripts/controllers/example-tab-two.js',
+   'plugins/docker-volume-plugin/scripts/controllers/wizard/test-page-one.js',
+   'plugins/docker-volume-plugin/scripts/controllers/wizard/test-page-two.js',
+   'plugins/docker-volume-plugin/scripts/services/example.js',
+   'plugins/docker-volume-plugin/scripts/services/example-dialog.js',
+   'plugins/docker-volume-plugin/scripts/services/example-wizard.js',
+   'plugins/docker-volume-plugin/scripts/services/example-context-menu.js'
 ], function(
    angular,
    ExampleMainController,
@@ -40,7 +40,7 @@ define([
       var translate = $filter('translate');
 
       PluginService.register({
-         name: 'esxui-example-plugin',
+         name: 'docker-volume-plugin',
          version: '1.0.0',
          api: '=1.0.0',
          stylesheets: [
@@ -58,44 +58,44 @@ define([
          navigator: [{
             title: translate('example.menu.title'),
             icon: 'icon-example-menu',
-            state: 'host.esxui-example-plugin',
+            state: 'host.docker-volume-plugin',
             onContext: function (e) {
                $rootScope.contextMenu.show('example', ['object'], e);
             },
             children: [{
                title: translate('example.menu.titleChildOne'),
                icon: 'icon-example-menu',
-               state: 'host.esxui-example-plugin.one',
+               state: 'host.docker-volume-plugin.one',
                onContext: function (e) {
                   $rootScope.contextMenu.show('example', ['object'], e);
                }
             }, {
                title: translate('example.menu.titleChildTwo'),
                icon: 'icon-example-menu',
-               state: 'host.esxui-example-plugin.two',
+               state: 'host.docker-volume-plugin.two',
                onContext: function (e) {
                   $rootScope.contextMenu.show('example', ['object'], e);
                }
             }]
          }],
          states: [{
-            name: 'host.esxui-example-plugin',
+            name: 'host.docker-volume-plugin',
             options: {
                url: '/example',
                views: {
                   'content@host': {
                      controller: 'ExampleMainController',
-                     templateUrl: 'plugins/esxui-example-plugin/views/example-main.html'
+                     templateUrl: 'plugins/docker-volume-plugin/views/example-main.html'
                   }
                }
             }
          }, {
-            name: 'host.esxui-example-plugin.one',
+            name: 'host.docker-volume-plugin.one',
             options: {
                url: '/one'
             }
          }, {
-            name: 'host.esxui-example-plugin.two',
+            name: 'host.docker-volume-plugin.two',
             options: {
                url: '/two'
             }
