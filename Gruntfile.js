cde751d272c9aa7dca544975a4ecbf8100945f07
Mark Soper <msoper@vmware.com>
2016-08-31 14:31:06 -0400

    dev build env option

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,38 @@ module.exports = function (grunt) {
    // Time how long tasks take. Can help when optimizing build times
    require('time-grunt')(grunt);
 
+   // prepare copyfiles and uglyfiles conditioned on env option
+
+   var env = grunt.option('env');
+
+   var copyfiles = [
+      'views/**/{,*/}*.html',
+      'templates/**/{,*/}*.html',
+      'images/**/{,*/}*.*',
+      'styles/fonts/**/{,*/}*.*',
+      'i18n/**/{,*/}*.*'
+   ];
+
+   if (env == 'dev') {
+     copyfiles.concat([
+       'scripts/**/{,*/}*.*',
+       'plugin.js'
+     ]);
+   }
+
+   var uglyfiles = (env === 'dev') ? {} : {
+     'build/dist/scripts/services/example.js': ['scripts/services/example.js'],
+     'build/dist/scripts/services/example-dialog.js': ['scripts/services/example-dialog.js'],
+     'build/dist/scripts/services/example-wizard.js': ['scripts/services/example-wizard.js'],
+     'build/dist/scripts/services/example-context-menu.js': ['scripts/services/example-context-menu.js'],
+     'build/dist/scripts/controllers/example-main.js': ['scripts/controllers/example-main.js'],
+     'build/dist/scripts/controllers/example-tab-one.js': ['scripts/controllers/example-tab-one.js'],
+     'build/dist/scripts/controllers/example-tab-two.js': ['scripts/controllers/example-tab-two.js'],
+     'build/dist/scripts/controllers/wizard/test-page-one.js': ['scripts/controllers/wizard/test-page-one.js'],
+     'build/dist/scripts/controllers/wizard/test-page-two.js': ['scripts/controllers/wizard/test-page-two.js'],
+     'build/dist/plugin.js': ['plugin.js']
+   };
+
    // Define the configuration for all the tasks
    grunt.initConfig({
       // Compiles Sass to CSS and generates necessary files if requested
@@ -21,18 +53,7 @@ module.exports = function (grunt) {
       },
       uglify: {
          dist: {
-           files: {
-             'build/dist/scripts/services/example.js': ['scripts/services/example.js'],
-             'build/dist/scripts/services/example-dialog.js': ['scripts/services/example-dialog.js'],
-             'build/dist/scripts/services/example-wizard.js': ['scripts/services/example-wizard.js'],
-             'build/dist/scripts/services/example-context-menu.js': ['scripts/services/example-context-menu.js'],
-             'build/dist/scripts/controllers/example-main.js': ['scripts/controllers/example-main.js'],
-             'build/dist/scripts/controllers/example-tab-one.js': ['scripts/controllers/example-tab-one.js'],
-             'build/dist/scripts/controllers/example-tab-two.js': ['scripts/controllers/example-tab-two.js'],
-             'build/dist/scripts/controllers/wizard/test-page-one.js': ['scripts/controllers/wizard/test-page-one.js'],
-             'build/dist/scripts/controllers/wizard/test-page-two.js': ['scripts/controllers/wizard/test-page-two.js'],
-             'build/dist/plugin.js': ['plugin.js']
-           }
+           files: uglyfiles
          }
       },
       copy: {
@@ -40,13 +61,7 @@ module.exports = function (grunt) {
             files: [{
                expand: true,
                dest: 'build/dist',
-               src: [
-                  'views/**/{,*/}*.html',
-                  'templates/**/{,*/}*.html',
-                  'images/**/{,*/}*.*',
-                  'styles/fonts/**/{,*/}*.*',
-                  'i18n/**/{,*/}*.*'
-               ]
+               src: copyfiles
             }]
          }
       }
@@ -57,4 +72,5 @@ module.exports = function (grunt) {
       'uglify',
       'copy'
    ]);
+
 };
