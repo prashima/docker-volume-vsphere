6707123d8e62cf4dd121638bf173ffb8a3885537
Mark Soper <msoper@vmware.com>
2016-09-01 16:33:28 -0400

    datastores grid

--- scripts/controllers/example-tab-one.js
+++ scripts/controllers/example-tab-one.js
@@ -28,13 +28,58 @@ define(['angular'], function(angular) {
       //       actionButton
       //    ]
       // };
+      var tenants = [
+        ["692c1e66-a3f4-4322-a4fb-85276659c9b9", "my-example-tenant-1", "I created this as an example"],
+        ["1e758b16-6f4d-4155-994d-f966f1805115", "my-example-tenant-2", "Another example tenant"],
+        ["b2303090-80e5-4efb-b2ea-763b86bd2d8b", "my-example-tenant-3", "This is one of my favorite test tenants"],
+        ["e0422589-7ae8-4651-abdf-12cecdd41cce", "my-example-tenant-3", "This is one of my favorite test tenants"],
+        ["692c1e66-a3f4-4322-a4fb-85276659c9b9", "my-example-tenant-1", "I created this as an example"],
+        ["1e758b16-6f4d-4155-994d-f966f1805115", "my-example-tenant-2", "Another example tenant"],
+        ["b2303090-80e5-4efb-b2ea-763b86bd2d8b", "my-example-tenant-3", "This is one of my favorite test tenants"],
+        ["e0422589-7ae8-4651-abdf-12cecdd41cce", "my-example-tenant-3", "This is one of my favorite test tenants"]
+      ];
 
-      $scope.gridSettings = {
-        data: [
-          {id: 123, name: "name 123"},
-          {id: 456, name: "name 456"}
-        ]
+      var datastores = [
+        ["99665316-9a27-46c2-a0ab-92103857c86b", "3GB", "1GB", false, false],
+        ["e0422589-7ae8-4651-abdf-12cecdd41cce", "1TB", "250GB", false, true],
+      ]
+
+      $scope.tenantsGridSettings = {
+        columns: [
+          {field:'ID',title:'ID'},//,rowspan:2,width:80,sortable:true}
+          {field:'name', title:'name'},
+          {field:'description', title:'description-foo'}
+        ],
+        data: tenants.map(function(row) {
+          return {
+            ID: row[0],
+            name: row[1],
+            description: row[2]
+          };
+        })
       };
+
+
+      $scope.datastoresGridSettings = {
+        columns: [
+          {field:'ID',title:'ID'},//,rowspan:2,width:80,sortable:true}
+          {field:'capacity', title:'Capacity'},
+          {field:'availability', title:'Global Availability'},
+          {field:'create', title:'Create', checkbox: true},
+          {field:'delete', title:'Delete', checkbox: true}
+        ],
+        data: datastores.map(function(row) {
+          return {
+            ID: row[0],
+            capacity: row[1],
+            availability: row[2],
+            create: row[3],
+            delete: row[4]
+          };
+        })
+      };
+
+
 
 
 
