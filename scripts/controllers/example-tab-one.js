676444a61475e9e179a44a23eaa642cf1600d152
Mark Soper <msoper@vmware.com>
2016-09-01 15:23:56 -0400

    trying new grid approach

--- scripts/controllers/example-tab-one.js
+++ scripts/controllers/example-tab-one.js
@@ -5,28 +5,39 @@ define(['angular'], function(angular) {
       $log, $rootScope, $scope,
       ExampleService, ExampleContextMenuService) {
 
-      $log = $log.getInstance('ExampleTabOneController', true);
-      $log.debug('In example tab one controller global scope');
+      // $log = $log.getInstance('ExampleTabOneController', true);
+      // $log.debug('In example tab one controller global scope');
+      //
+      // var actionButton = {
+      //    id: 'exampleButton',
+      //    label: 'Test button one',
+      //    tooltipText: 'Test tooltip',
+      //    enabled: true,
+      //    iconClass: 'esx-icon-example',
+      //    onClick: function (e) {
+      //       var button = $(e.currentTarget);
+      //       var pos = button.offset();
+      //       var height = button.height();
+      //       $rootScope.contextMenu.show('example', ['object'], e,
+      //          pos.left - 1, pos.top + height + 4);
+      //    }
+      // };
+      //
+      // $scope.actionBar = {
+      //    actions: [
+      //       actionButton
+      //    ]
+      // };
 
-      var actionButton = {
-         id: 'exampleButton',
-         label: 'Test button one',
-         tooltipText: 'Test tooltip',
-         enabled: true,
-         iconClass: 'esx-icon-example',
-         onClick: function (e) {
-            var button = $(e.currentTarget);
-            var pos = button.offset();
-            var height = button.height();
-            $rootScope.contextMenu.show('example', ['object'], e,
-               pos.left - 1, pos.top + height + 4);
-         }
+      $scope.gridSettings = {
+        data: [
+          {id: 123, name: "name 123"},
+          {id: 456, name: "name 456"}
+        ]
       };
 
-      $scope.actionBar = {
-         actions: [
-            actionButton
-         ]
-      };
+
+
+
    };
 });
