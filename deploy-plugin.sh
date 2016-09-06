29b662577eef195f5a5d3bc25448068e0bfa094c
Mark Soper <msoper@vmware.com>
2016-09-06 16:30:05 -0400

    cleaned out all original started components

--- deploy-plugin.sh
+++ deploy-plugin.sh
@@ -4,9 +4,11 @@
 
 # TODO: make these vars command line params
 
+rm -rf build/dist/*
 grunt --env=dev
 ESX_IP=192.168.73.131
 SRC_PATH=.
 PLUGIN_NAME=docker-volume-plugin
 BUILD_PATH=${SRC_PATH}/build/dist
-scp -r ${BUILD_PATH}/* root@${ESX_IP}:/usr/lib/vmware/hostd/docroot/ui/plugins/${PLUGIN_NAME}/
+ssh root@${ESX_IP} rm -rf /usr/lib/vmware/hostd/docroot/ui/plugins/${PLUGIN_NAME}/
+scp -r ${BUILD_PATH}/. root@${ESX_IP}:/usr/lib/vmware/hostd/docroot/ui/plugins/${PLUGIN_NAME}/
--- plugin.js
+++ plugin.js
@@ -1,43 +1,25 @@
 
 define([
    'angular',
-   'plugins/docker-volume-plugin/scripts/controllers/dvol-main.js',
-   'plugins/docker-volume-plugin/scripts/controllers/tenants-list.js',
-   'plugins/docker-volume-plugin/scripts/controllers/dvol-tab-two.js',
-   'plugins/docker-volume-plugin/scripts/controllers/wizard/test-page-one.js',
-   'plugins/docker-volume-plugin/scripts/controllers/wizard/test-page-two.js',
-   'plugins/docker-volume-plugin/scripts/services/dvol.js',
-   'plugins/docker-volume-plugin/scripts/services/dvol-dialog.js',
-   'plugins/docker-volume-plugin/scripts/services/dvol-wizard.js',
-   'plugins/docker-volume-plugin/scripts/services/dvol-context-menu.js'
+   'plugins/docker-volume-plugin/scripts/controllers/dvol-main.js'
 ], function(
    angular,
-   DvolMainController,
-   DvolTenantsListController,
-   DvolTabTwoController,
-   DvolWizardTestPageOneController,
-   DvolWizardTestPageTwoController,
-   DvolService,
-   DvolDialogService,
-   DvolWizardService,
-   DvolContextMenuService) {
+   DvolMainController) {
 
    'use strict';
 
    return angular.module('esxUiApp.plugins.dvol', [
       'ui.router',
    ]).controller({
-      'DvolMainController': DvolMainController,
-      'DvolTenantsListController': DvolTenantsListController,
-      'DvolTabTwoController': DvolTabTwoController,
-      'DvolWizardTestPageOneController': DvolWizardTestPageOneController,
-      'DvolWizardTestPageTwoController': DvolWizardTestPageTwoController
-   }).service({
-      'DvolService': DvolService,
-      'DvolDialogService': DvolDialogService,
-      'DvolWizardService': DvolWizardService,
-      'DvolContextMenuService': DvolContextMenuService
-   }).run(function ($rootScope, $filter, PluginService) {
+      'DvolMainController': DvolMainController
+   })
+  //  .service({
+  //     'DvolService': DvolService,
+  //     'DvolDialogService': DvolDialogService,
+  //     'DvolWizardService': DvolWizardService,
+  //     'DvolContextMenuService': DvolContextMenuService
+  //  })
+   .run(function ($rootScope, $filter, PluginService) {
       var translate = $filter('translate');
 
       PluginService.register({
@@ -47,32 +29,39 @@ define([
          stylesheets: [
             'styles/main.css'
          ],
-         contextMenuServices: [
-            'DvolContextMenuService'
-         ],
-         dialogServices: [
-            'DvolDialogService'
-         ],
-         wizardServices: [
-            'DvolWizardService'
-         ],
-         navigator: [{
-            title: translate('dvol.menu.title'),
-            icon: 'icon-example-menu',
-            state: 'host.docker-volume-plugin',
-            onContext: function (e) {
-               $rootScope.contextMenu.show('dvol', ['object'], e);
-            },
-            children: [{
-               title: translate('dvol.menu.titleChildOne'),
-               icon: 'icon-example-menu',
-               state: 'host.docker-volume-plugin.one',
-               onContext: function (e) {
-                  $rootScope.contextMenu.show('dvol', ['object'], e);
-               }
-            }]
-         }],
-         states: [{
+         //
+         // register services in here, e.g.
+         //
+        //  contextMenuServices: [
+        //     'DvolContextMenuService'
+        //  ],
+        //  dialogServices: [
+        //     'DvolDialogService'
+        //  ],
+        //  wizardServices: [
+        //     'DvolWizardService'
+        //  ],
+         //
+         //
+         //
+        //  navigator: [{
+        //     title: translate('dvol.menu.title'),
+        //     icon: 'icon-example-menu',
+        //     state: 'host.docker-volume-plugin',
+        //     onContext: function (e) {
+        //        $rootScope.contextMenu.show('dvol', ['object'], e);
+        //     },
+        //     children: [{
+        //        title: translate('dvol.menu.titleChildOne'),
+        //        icon: 'icon-example-menu',
+        //        state: 'host.docker-volume-plugin.one',
+        //        onContext: function (e) {
+        //           $rootScope.contextMenu.show('dvol', ['object'], e);
+        //        }
+        //     }]
+        //  }],
+         states: [
+           {
             name: 'host.docker-volume-plugin',
             options: {
                url: '/docker-volume-plugin',
@@ -83,17 +72,19 @@ define([
                   }
                }
             }
-         }, {
-            name: 'host.docker-volume-plugin.one',
-            options: {
-               url: '/one'
-            }
-         }, {
-            name: 'host.docker-volume-plugin.two',
-            options: {
-               url: '/two'
-            }
-         }]
+         }
+        //  , {
+        //     name: 'host.docker-volume-plugin.one',
+        //     options: {
+        //        url: '/one'
+        //     }
+        //  }, {
+        //     name: 'host.docker-volume-plugin.two',
+        //     options: {
+        //        url: '/two'
+        //     }
+        //  }
+         ]
       });
    });
 });
