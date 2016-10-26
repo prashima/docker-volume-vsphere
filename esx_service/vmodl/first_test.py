22ce7e5ef618f748fd6b3cf5c3a79d40c4a6973d
Mark Sterin <msterin@vmware.com>
2016-10-25 17:51:06 -0700

    Cleanup of vmodl example - comments and localhost name

--- esx_service/vmodl/first_test.py
+++ esx_service/vmodl/first_test.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# Copyright 2016 VMWare, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #
 # An example of Connecting to (local) VSAN SIMS and fetching simple data 
 # from VSAN and VsanDockerPersistentVolumeSystem
@@ -33,7 +48,8 @@ from vsanPerfPyMo import VsanPerformanceManager
 
 si = None
 
-def connect_to_vsanmgmt(host = "localhost.localdomain", port = 443):
+def connect_to_vsanmgmt(host = "localhost", port = 443):
+
     """
     Connects to VSAN mgmt service on ESX (/vsan) and returns SOAP stub
     to use
